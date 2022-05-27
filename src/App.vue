3d1e6f1d0ffddffbda69746fc875d00d8f7b4ed3
Shaqnificent <shaqnificent@gmail.com>
2022-05-27 18:04:43 -0500

    updated views and router

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,55 @@
 <template>
   <div class="app">
-    <h1>Hello, world</h1>
+    <Sidebar />
+    <router-view />
   </div>
 
 </template>
 
+<script setup>
+import Sidebar from './components/Sidebar.vue'
+</script>
+
 <style lang="scss">
+:root {
+  --primary: blue;
+  --primary-alt: lightBlue;
+  --grey: grey;
+  --dark: black;
+  --dark-alt: darkGrey;
+  --light: lightGrey;
+  --sidebar-width: 300px;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira sans', sans-serif;
+}
+
+body {
+  background: var(--light);
+}
+
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+.app {
+  display: flex;
+
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+
+    @media (max-width: 768px){
+      padding-left: 6rem;
+    }
+  }
+}
 
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,12 @@
+<template lang="">
+   <aside>
+       sidebar
+   </aside>
+</template>
+
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+    
+</style>
