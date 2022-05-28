df9c4bb1e28ad9b2f623605194248c515e87a7d0
Shaqnificent <shaqnificent@gmail.com>
2022-05-28 08:37:25 -0500

    Updated the sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,12 +1,79 @@
 <template lang="">
-   <aside>
-       sidebar
+   <aside :class="`${is_expanded ? 'is-expanded' : ''}`">
+       <div class="logo">
+           <img src="../assets/logo.png" alt="">
+       </div>
+
+       <div class="menu-toggle-wrap">
+           <button class="menu-toggle" @click="ToggleMenu">
+               <span class="material-icons" >keyboard_double_arrow_right</span>
+           </button>
+       </div>
    </aside>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 
+const is_expanded = ref(false)
+
+const ToggleMenu = () => {
+    is_expanded.value = !is_expanded.value
+}
 </script>
 <style lang="scss" scoped>
-    
+aside {
+    display: flex;
+    flex-direction: column;
+    //width: var(--sidebar-width);
+    width: calc(2rem + 32px);
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 1rem;
+
+    background-color: var(--dark);
+    color: var(--light);
+
+    transition: 0.2s ease-out;
+
+    .logo {
+        margin-bottom: 1rem;
+
+        img {
+            width: 2rem;
+        }
+    }
+    .menu-toggle-wrap {
+        display: flex;
+        justify-content: flex-end;
+        margin-bottom: 1rem;
+
+        position: relative;
+        top: 0;
+        transition: 0.2s ease-out;
+
+        .menu-toggle {
+            transition: 0.2s ease-out;
+
+            .material-icons {
+                font-size: 2rem;
+                color: var(--light);
+            }
+        }
+    }
+    &.is-expanded {
+        width: var(--sidebar-width);
+
+        .menu-toggle-wrap {
+            top: -3rem;
+            .menu-toggle {
+                transform: rotate(-180deg);
+            }
+        }
+    }
+    @media (max-width: 768px) {
+        position:fixed;
+        z-index: 99;
+    }
+}
 </style>
